00340a763d49eee938fe537b3970378b47f7e5a7
Nipun Theekshana <theekshana2@gmail.com>
2022-11-16 13:08:01 +0530

    smart contract read and write functions

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   const CONTRACT_ADDRESS = '0xDB24115ebFf916FCb1a2E4280acf8e9e6cF3B2Ea'
   const CONTRACT_ABI = '[{"inputs":[],"name":"count","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"dec","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"get","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"inc","outputs":[],"stateMutability":"nonpayable","type":"function"}]'
 
-  const { account, activate, deactivate } = useWeb3React()
+  const { account, activate, deactivate, library } = useWeb3React()
   const [currentBlockNumber, setCurrentBlockNumber] = useState(0)
   const [currentCounterValue, setCurrentCounterValue] = useState(0)
 
@@ -43,6 +43,28 @@ function App() {
     readCurrentCounterValue()
   }, [])
 
+  const handleDecrements = async () => {
+    const provider = await getWeb3Provider()
+    const contractInstance = new ethers.Contract(CONTRACT_ADDRESS, JSON.parse(CONTRACT_ABI), provider)
+    const signer = library.getSigner()
+    const contractInstanceWithSigner = contractInstance.connect(signer)
+    const txReceipt = await contractInstanceWithSigner.dec()
+    const result = await txReceipt.wait()
+    console.log("result", result)
+    readCurrentCounterValue()
+  }
+
+  const handleIncrements = async () => {
+    const provider = await getWeb3Provider()
+    const contractInstance = new ethers.Contract(CONTRACT_ADDRESS, JSON.parse(CONTRACT_ABI), provider)
+    const signer = library.getSigner()
+    const contractInstanceWithSigner = contractInstance.connect(signer)
+    const txReceipt = await contractInstanceWithSigner.inc()
+    const result = await txReceipt.wait()
+    console.log("result", result)
+    readCurrentCounterValue()
+  }
+
   return (
     <div className="App">
       <div className='metamask-login-container' style={{ marginTop: '50px' }}>
@@ -55,9 +77,12 @@ function App() {
                 Account Address : <b>{account}</b>
               </div>
 
-              <div className='logout-container' style={{ marginTop: '10px' }}>
+              <div className='logout-container' style={{ marginTop: '10px', marginBottom: '20px' }}>
                 <button onClick={handleDisconnect}>Disconnect</button>
               </div>
+
+              <button onClick={handleDecrements}>Dec</button>
+              <button onClick={handleIncrements}>Inc</button>
 
             </div>
           ) : (
